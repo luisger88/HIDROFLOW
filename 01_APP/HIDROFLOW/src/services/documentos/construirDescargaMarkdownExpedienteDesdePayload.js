064107758c0191c15a7780abf9bd6e24fd136851
Luis G. Montoya <luisger88@hotmail.com>
2026-06-20 21:51:24 -0500

    feat(expediente): conecta payload a descarga markdown

---
+++ 01_APP/HIDROFLOW/src/services/documentos/construirDescargaMarkdownExpedienteDesdePayload.js
@@ -0,0 +1,16 @@
+import construirMarkdownExpedienteDesdePayload from "./construirMarkdownExpedienteDesdePayload.js";
+import prepararDescargaMarkdownExpediente from "./prepararDescargaMarkdownExpediente.js";
+
+export default function construirDescargaMarkdownExpedienteDesdePayload(payload = {}) {
+  const markdown = construirMarkdownExpedienteDesdePayload(payload);
+
+  const nombreCuenca =
+    payload?.cuenca?.nombre ||
+    payload?.cuenca?.puntoSalida?.id ||
+    "expediente_hidrologico_minimo";
+
+  return prepararDescargaMarkdownExpediente({
+    markdown,
+    nombreBase: nombreCuenca
+  });
+}
